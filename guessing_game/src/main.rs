0c7942394ed5d630ece081a2fc867c0c7b7b570b
ArchKudo <vgd.vyas17@gmail.com>
2017-12-30 11:43:42 +0530

    Generated random number, removed debug println

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,20 @@
+extern crate rand;
+
 // io is a library inside the std library for I/O
 use std::io;
+use rand::Rng;
 
 fn main() {
 	// Add a prompt
     println!("Guess a number!");
-    println!("Please input your guess!");
+
+    // `thread_rng` provide os seeded thread-local random number
+    // `gen_range` provides random number between [lower, upper)
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess!");	
 
     // `let` is used to define new variables
     // `::new` is an associated function a.k.a static method
@@ -13,8 +23,8 @@ fn main() {
 
     // `read_line` returns either Ok/Err
     // `expect` panics on Err or returns number of bytes on Ok 
-    let val = io::stdin().read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
     	.expect("Failed to read line!");
 
-    println!("You guessed: {}, {}", guess, val);
+    println!("You guessed: {}", guess);
 }
